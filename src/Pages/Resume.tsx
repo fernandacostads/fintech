fd560ab0ce2b723d76c28f0d9a441e87854800e8
Fernanda Costa <fernandacosta@alu.ufc.br>
2024-03-29 11:26:21 -0300

    feat: add pages, Hooks and Context

---
+++ src/Pages/Resume.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useData } from '../Context/DataContext'
+
+const Resume = () => {
+  const {data} = useData();
+  return (
+   <div>Resume</div>
+
+  )
+}
+
+export default Resume
